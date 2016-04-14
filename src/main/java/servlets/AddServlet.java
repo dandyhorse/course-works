3519e96b56b786ed207b78df01589051bbeb055d
tonycox <tonycox@bk.ru>
2016-04-14 23:21:16 +0400

    add adding users and encription their passwords

--- src/main/java/servlets/AddServlet.java
+++ src/main/java/servlets/AddServlet.java
@@ -1,9 +1,14 @@
 package servlets;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import hibernate.DaoAccessor;
+import hibernate.entity.Admin;
+import hibernate.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,13 +18,68 @@ import java.io.IOException;
 /**
  * Created by User on 04.04.2016.
  */
-@RequestMapping("/add")
+@Configurable
 public class AddServlet extends HttpServlet {
 
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
+    @Autowired
+    private DaoAccessor daoAccessor;
 
+    public void setDaoAccessor(DaoAccessor daoAccessor) {
+        this.daoAccessor = daoAccessor;
     }
 
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String password = req.getParameter("password");
+        String option = req.getParameter("option");
+        String login = req.getParameter("login");
+
+        if (isFillInputs(option, login, password)) {
+            String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+            if (addByOption(option, login, hashedPassword)) {
+                answer(req, resp, HttpServletResponse.SC_OK, "ACCEPTED!");
+            } else {
+                answer(req, resp, HttpServletResponse.SC_BAD_REQUEST, "ERROR IN INPUT");
+            }
+        } else {
+            answer(req, resp, HttpServletResponse.SC_BAD_REQUEST, "ERROR IN INPUT");
+        }
+
+        req.getRequestDispatcher("add.jsp").forward(req, resp);
+    }
+
+    private void answer(HttpServletRequest req, HttpServletResponse resp, int scBadRequest, String o) {
+        req.setAttribute("message", o);
+        resp.setStatus(scBadRequest);
+    }
+
+    private boolean isFillInputs(String option, String login, String password) {
+        boolean bool = (option != null && !option.isEmpty());
+        bool = bool && (login != null && !login.isEmpty());
+        bool = bool && (password != null && !password.isEmpty());
+        return bool;
+    }
+
+    private boolean addByOption(String option, String login, String hashedPassword) {
+        try {
+            if (option.equals("admin")) {
+                Admin admin = new Admin(login, hashedPassword);
+                daoAccessor.getAdminDao().add(admin);
+            }
+            if (option.equals("user")) {
+                User user = new User(login, hashedPassword);
+                daoAccessor.getUserDao().add(user);
+            }
+        } catch (Exception e) {
+            e.getMessage();
+            return false;
+        }
+        return true;
+    }
 
 }
--- src/main/webapp/add.jsp
+++ src/main/webapp/add.jsp
@@ -1,10 +1,13 @@
-<%--
+<%--@elvariable id="message" type="java.lang.String"--%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%--@elvariable id="Admin" type="hibernate.entity.Admin"--%>
+<%@ page import="hibernate.entity.Admin" %><%--
   Created by IntelliJ IDEA.
   User: User
   Date: 06.04.2016
   Time: 1:12
 --%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" errorPage="error.jsp"%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" errorPage="error.jsp" %>
 <html>
 <head>
     <title>Title</title>
@@ -13,18 +16,35 @@
 
 <aside>
 
-    <a href="add.jsp">Добавить</a>
-    </br></br>
-    <a href="index.jsp">Назад</a>
+    <a href="index.jsp">На Главную</a>
+    <br><br>
+
+
 </aside>
 
 <h1>Добавление</h1>
 
-<form action="${pageContext.request.contextPath}/add" method="post">
+<script type="text/javascript">
+    function OnDropDownChange(dropDown) {
+        document.getElementById("selectedOption").value = dropDown.options[dropDown.selectedIndex].value;
+    }
+</script>
 
-    <input type="text" name="" title="login">
-    <input type="password" name="" title="password">
+<form action="${pageContext.request.contextPath}/add"
+      method="post"
+      accept-charset="UTF-8">
+    <select onChange="OnDropDownChange(this);" title="What to add">
+        <option value="admin">Admin</option>
+        <option value="user">User</option>
+    </select>
+
+    <input type="hidden" name="option" id="selectedOption" value="admin">
+    <input type="text" name="login" title="login">
+    <input type="password" name="password" title="password">
     <input type="submit">
+    <p>${message}</p>
+
+    <a href="<c:url value="/results"/>">Показать все данные</a>
 
 </form>
 
