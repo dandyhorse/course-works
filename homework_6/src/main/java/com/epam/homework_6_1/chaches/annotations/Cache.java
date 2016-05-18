7511032321170a0b6466098cb2450792cb749e84
tonycox <tonycox@bk.ru>
2016-05-18 18:07:20 +0400

    rename and add impl

--- homework_6/src/main/java/com/epam/homework_6_1/chaches/annotations/Cache.java
+++
@@ -1,5 +0,0 @@
-package com.epam.homework_6_1.chaches.annotations;
-
-public @interface Cache {
-    String name();
-}
--- homework_6/src/main/java/com/epam/homework_6_1/chaches/interfaces/ICache.java
+++
@@ -1,7 +0,0 @@
-package com.epam.homework_6_1.chaches.interfaces;
-
-public interface ICache<T> {
-    T get();
-
-    void set(T t);
-}
