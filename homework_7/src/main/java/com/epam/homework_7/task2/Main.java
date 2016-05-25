b6bf1f29f0b5e682b445b468743e504f3b69bf0f
tonycox <tonycox@bk.ru>
2016-05-26 03:28:32 +0400

    main and abstract

---
+++ homework_7/src/main/java/com/epam/homework_7/task2/Main.java
@@ -0,0 +1,16 @@
+package com.epam.homework_7.task2;
+
+import com.epam.homework_7.task2.a.RunnerA;
+import com.epam.homework_7.task2.b.RunnerB;
+
+
+public class Main {
+    public static void main(String[] args) {
+        //2a task. solution with facilities under java 1.5
+//        new RunnerA().start();
+
+        //2b task. solution with facilities over java 1.5
+//        new RunnerB().start();
+    }
+
+}
