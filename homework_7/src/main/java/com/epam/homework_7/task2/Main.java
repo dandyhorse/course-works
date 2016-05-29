24f2339e2b6bd280422a128d71dbda07c273d076
Solovev Anton <tonycox@bk.ru>
2016-05-29 22:02:29 +0400

    Update Main.java

--- homework_7/src/main/java/com/epam/homework_7/task2/Main.java
+++ homework_7/src/main/java/com/epam/homework_7/task2/Main.java
@@ -7,10 +7,10 @@ import com.epam.homework_7.task2.b.RunnerB;
 public class Main {
     public static void main(String[] args) {
         //2a task. solution with facilities under java 1.5
-//        new RunnerA().start();
+        new RunnerA().start();
 
         //2b task. solution with facilities over java 1.5
-//        new RunnerB().start();
+        new RunnerB().start();
     }
 
 }
