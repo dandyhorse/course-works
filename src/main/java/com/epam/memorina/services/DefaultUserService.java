bedfe43cb6475d25288de119f0b9664c82b54336
tonycox <tonycox@bk.ru>
2016-08-01 00:58:35 +0400

    add service layer, and create a few methods

--- src/main/java/com/epam/memorina/services/DefaultUserService.java
+++ src/main/java/com/epam/memorina/services/DefaultUserService.java
@@ -1,29 +1,61 @@
 package com.epam.memorina.services;
 
+import com.epam.memorina.assemblers.Assembler;
 import com.epam.memorina.entities.UserEntity;
+import com.epam.memorina.models.User;
 import com.epam.memorina.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author Solovev Anton
  * @since 30.07.2016.
  */
-@Service("defaultService")
+@Service
+@Transactional
 public class DefaultUserService {
 
-    @Autowired
     private UserRepository userRepository;
+    private Assembler<User, UserEntity> assembler;
 
-    public List<UserEntity> loadUsers() {
-        List<UserEntity> userEntityList = (List<UserEntity>) userRepository.findAll();
-        return userEntityList;
+    public DefaultUserService(@Autowired UserRepository userRepository,
+                              @Autowired Assembler<User, UserEntity> assembler) {
+        this.userRepository = userRepository;
+        this.assembler = assembler;
     }
 
-    public void saveUser(UserEntity userEntity) {
+    public List<User> loadAll() {
+        List<UserEntity> allEntities = (List<UserEntity>) userRepository.findAll();
+        return allEntities
+                .stream()
+                .map(this::newModel)
+                .collect(Collectors.toList());
+    }
+
+    public void save(User user) {
+        UserEntity userEntity = newEntity(user);
         userRepository.save(userEntity);
     }
 
+    public User loadByName(String username) {
+        UserEntity user = userRepository.findByUsername(username);
+        return newModel(user);
+    }
+
+    public boolean validUser(User user) {
+        User userFromRepository = loadByName(user.getUsername());
+        return user.getPassword().equals(userFromRepository.getPassword());
+    }
+
+    private User newModel(UserEntity entity) {
+        return assembler.newModel(entity);
+    }
+
+    private UserEntity newEntity(User user) {
+        return assembler.newEntity(user);
+    }
 }
