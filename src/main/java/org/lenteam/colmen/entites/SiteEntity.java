d36c69fcf0b7c21567c4b3d9593b059cfd649262
RinxSR <rinxsr@gmail.com>
2016-08-30 10:23:11 +0300

    Add SiteEntity

--- src/main/java/org/lenteam/colmen/entites/SiteEntity.java
+++ src/main/java/org/lenteam/colmen/entites/SiteEntity.java
@@ -1,4 +1,49 @@
 package org.lenteam.colmen.entites;
 
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "Sites")
 public class SiteEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "site")
+    private Set<PageEntity> pages = new HashSet<>();
+
+    public SiteEntity() {
+        //no args constructor
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<PageEntity> getPages() {
+        return pages;
+    }
+
+    public void setPages(Set<PageEntity> pages) {
+        this.pages = pages;
+    }
 }
